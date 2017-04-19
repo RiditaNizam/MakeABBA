d6319c7849b118a9ad520442c53f0065b76ec9df
RiditaNizam <riditan@gmail.com>
2017-04-19 00:42:29 -0400

    Initial commit

---
+++ src/com/company/MakeABBA.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by ridita on 4/19/17.
+ */
+public class MakeABBA {
+    public String makeAbba(String a, String b) {
+        return (a + b + b + a);
+    }
+
+}
